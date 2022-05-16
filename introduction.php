06de5a273226b0e3f4680c7bbcd327baf99c2436
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-16 09:48:41 +0200

    CREATE

---
+++ introduction.php
@@ -0,0 +1,14 @@
+<!-- Balises Php -->
+<!-- L'ensemble du code sera entouré par des PHP. -->
+<!-- La balise echo fera un syso -->
+
+
+<?php 
+    echo '<h1>Hello World !</h1>';
+?>
+
+<!-- Afficher directement dans echo -->
+<?= ... ?>
+
+<!-- Attention on ne peut pas créer le projet n'importe ou.
+il faudra aller directement dans le dossier d'installation de Wamp -->
