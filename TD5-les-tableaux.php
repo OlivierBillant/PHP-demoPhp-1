2d2b8188dcf976481874830401b7774680c17c47
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-16 12:03:33 +0200

    CHANGE TD5 encore

--- TD5-les-tableaux.php
+++ TD5-les-tableaux.php
@@ -10,7 +10,17 @@ var_dump($tab);
 //Permet d'afficher le tableau sans le détail du debug.
 print_r($tab);
 
-$etudiant[5] = "Pierre";
-$etudiant[20] = "Gael";
-$etudiant[3] = "Plop";
-var_dump($etudiant);
+//Si l'on précise l'indice, le tableau sera non ordonnée
+$etudiants[5] = "Pierre";
+$etudiants[20] = "Gael";
+$etudiants[3] = "Plop";
+var_dump($etudiants);
+
+//Les tableaux associatifs : passe une clef en chaine de caractere
+//On pourra lui passer directement en tableau par la variable
+$classe["QCDA_006F"] = $etudiants;
+//Ou passer un tableau par creation avec array()
+$classe["QCDA_007F"] = array("Yannick", "Dominique", "Jérome", "Christelle");
+
+//L affichage de l array $classe contiendra 2 tableaux
+var_dump($classe);
