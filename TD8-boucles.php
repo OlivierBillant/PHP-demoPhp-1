ac5c35a60795a080e10c6631c03733ca3174a52a
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-16 14:45:35 +0200

    CHANGE TD3 et TD8

--- TD8-boucles.php
+++ TD8-boucles.php
@@ -13,54 +13,21 @@ Le foreach : foreach ($tableau as $valeur)
     foreach($table as $key => $val):
     <a class="nav-link active" aria-current="page" href="<?= $val?>.php"><?=$key?></a>
     */
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<?php include("TD8-parts/head.php") ?>
+ 
 </head>
 <body>
-    
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <?php foreach($menu as $elem): ?>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"><?= $elem ?></a>
-        </li>
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <?php endforeach; ?>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-</body>
-</html>
-
 <!-- Inclusion de fichier
 include :
 -->
+<?php include("TD8-parts/menu.php") ?>
+</body>
+</html>
+
+<!-- L'inclusion obligatoire utilisera "require" -->
+<!-- Des variants existent : include_once et require_once ce qui permet d'éviter des boucles d'inclusion. -->
---
+++ TD8-pages/contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("./TD8-parts/head.php") ?>
+</head>
+
+<body>
+    <?php include('./TD8-parts/menu.php'); ?>
+</body>
+
+</html>
