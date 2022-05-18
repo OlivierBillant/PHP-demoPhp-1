a6041b46c9e26b24be2d888bd5c18f2a129e557e
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-18 13:46:08 +0200

    ADD : TD15

---
+++ TD15-personne.class.php
@@ -0,0 +1,81 @@
+<?php
+
+class Personne{
+    private $prenom;
+    private $nom;
+    private $age;
+
+
+    public function __construct($prenom, $nom, $age){
+        $this->prenom = $prenom;
+        $this->nom = $nom;
+        $this->age = $age;
+    }
+
+
+    public function __destruct()
+    {
+        var_dump($this->prenom);
+        echo "<br>L'objet est détruit à la fin du dernier appel php de la page";
+        echo "<br>Sauf si l'on appelle unset";
+    }
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of age
+     */ 
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @return  self
+     */ 
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+}
