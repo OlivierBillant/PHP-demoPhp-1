2e608c513e181a7f77f890a37ac68181e0b0c674
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-18 11:41:23 +0200

    CHANGE : Ajout filter

--- TD14-les-formulaires-php.php
+++ TD14-les-formulaires-php.php
@@ -2,4 +2,13 @@
 echo "PLOP";
 echo "<br>email : {$_POST['email']}";
 var_dump($_POST);
+
+$email = "olivier@guilben.bzh";
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo("$email is a valid email address");
+} else {
+  echo("$email is not a valid email address");
+}
+
 ?>
