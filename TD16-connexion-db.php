4c75cfca698b7e35743a0601e1c510225acbbed8
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-19 16:59:56 +0200

    CHANGE : Req SQL test

--- TD16-connexion-db.php
+++ TD16-connexion-db.php
@@ -6,8 +6,15 @@ try {
     $dbname = "db_php";
     $db = new PDO("mysql:host=$hote;dbname=$dbname", $username, $pw);
     echo "Connected";
-    var_dump($db->getAttribute(PDO::ATTR_SERVER_INFO));
+    var_dump($db->getAttribute(PDO::ATTR_SERVER_VERSION));
+
+    $sql = "SELECT * FROM STOCK";
+    $stmt = $db->query($sql);
+    // $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($arr);
+
+    $one = $stmt->fetch(PDO::FETCH_ASSOC);
+    var_dump($one);
 } catch (PDOException $e) {
     echo "Not Connected";
-    //throw $th;
 }
